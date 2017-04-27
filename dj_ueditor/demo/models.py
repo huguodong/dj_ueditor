b8fdd35996ad65cb6f6b8786f2c9c0a236adf198
Tiger <531035580@QQ.COM>
2017-04-27 14:45:29 +0800

    11

--- dj_ueditor/demo/models.py
+++ dj_ueditor/demo/models.py
@@ -1,9 +1,9 @@
-from django.db import models
+﻿from django.db import models
 from DjangoUeditor.models import UEditorField
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(u'标题',max_length=30)
     context = UEditorField(u'内容', height=300, width=1000,
                            default=u'', blank=True, imagePath='upload/images/', toolbars='besttome')
     def __str__(self):
